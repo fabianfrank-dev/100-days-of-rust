ed3ad44d30c3aca1045a5c33367a44ae6a73d02a
Fabian Frank <frankfabian945@outlook.de>
2026-06-04 20:20:11 +0300

    Added the note taking logic to the file read and write logic

--- getting_started/day-7/file_write_read/src/main.rs
+++ getting_started/day-7/file_write_read/src/main.rs
@@ -1,8 +1,78 @@
 use std::fs::File;
+use std::fs;
 use std::io::prelude::*;
 
-fn main() -> std::io::Result<()> {
-    let mut file = File::create("notes/foo.txt")?;
-    file.write_all(b"Hello, world!")?;
+ #[derive(Clone)]
+struct Note{
+    title: String,
+    content: String
+}
+
+enum Action{
+    Create(Note),
+    View(String),
+    Delete(String)
+}
+
+impl Action{
+    fn execute(&self, notes: &mut Vec<Note>){   
+        match self{
+            Action::Create(note) => {
+                let file = create_file(note.title.to_string());
+                let _ = write_into_file(file, &note.content.as_bytes());
+                println!("Note {} was successfully added", note.title);
+ 
+            },
+            Action::View(filename) => {
+                open_file(filename.to_string());
+            },
+            Action::Delete(filename) => {
+                let _ = delete_file(filename.to_string());
+                println!("File was successfully deleted");
+            },   
+        }
+    }
+}
+
+fn main(){
+    let note = Note{
+        title: "Test".to_string(),
+        content: "Hello from test".to_string()
+    };
+
+    let mut notes = Vec::new();
+
+    let create = Action::Create(note);
+    create.execute(&mut notes);
+}
+
+fn create_file(filename: String) -> std::io::Result<File> {
+    // create file
+    let path: String = format!("notes/{}.txt", filename);
+    println!("{}", path);
+    let file: std::io::Result<File> = File::create(path);
+
+    // return file
+    file
+}
+
+fn open_file(filename: String){
+    
+    let path: String = format!("notes/{}.txt", filename);
+    let contents = fs::read_to_string(path)
+        .expect("Should have been able to read the file");
+
+    println!("{}", contents);
+}
+
+fn write_into_file(file: std::io::Result<File>, content: &[u8]) -> std::io::Result<()>{
+    let _ = file?.write_all(content);
+    
+    Ok(())
+}
+
+fn delete_file(filename: String) -> std::io::Result<()>{
+    let path: String = format!("notes/{}.txt", filename);
+    fs::remove_file(path)?;
     Ok(())
 }
