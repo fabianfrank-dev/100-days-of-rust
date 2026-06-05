a8fe0ddb61841f7a790483d40872b65786d4ad4e
Fabian Frank <frankfabian945@outlook.de>
2026-06-05 19:58:11 +0300

    Added file writing features to shopping list

--- getting_started/day-8/shopping_list/src/main.rs
+++ getting_started/day-8/shopping_list/src/main.rs
@@ -1,53 +1,75 @@
-use std::collections::HashMap;
 use std::io;
+use std::fs::File;
+use std::io::Write;
 
 #[derive(Debug)]
 struct Products{
-    product: HashMap<String, u32>
+    item: String,
+    amount: u32
 }   
-impl Products{
-    fn view(&self){
-        // iterate through items to print
-        for item in &self.product{
-            println!("{:?}", item);
-        }
+
+fn view(products: &Vec<Products>){
+
+    // iterate through items to print
+    for item in products{
+        println!("{}: {}", item.item, item.amount);
     }
+    
+}
     // 
-    fn add(&mut self){
-        // initialise new input variables
-        let mut item: String = String::new();
-        let mut amount: String = String::new();
+    
+fn add(products: &mut Vec<Products>){
         
-        // ask for user input and get it
-        println!("Which item would you like to add?");
-
-        io::stdin()
-            .read_line(&mut item)
-            .expect("Failed to read line");
-
-        println!("Gotcha! How many items of '{}' do you need?", item.clone());
+    // initialise new input variables
+    let mut item: String = String::new();
+    let mut amount: String = String::new();
         
-        io::stdin()
-            .read_line(&mut amount)
-            .expect("Failed to read line");
+    // ask for user input and get it
+    println!("Which item would you like to add?");
+
+    io::stdin()
+        .read_line(&mut item)
+        .expect("Failed to read line");
+
+    println!("Gotcha! How many items of '{}' do you need?", item);
+    
+    io::stdin()
+        .read_line(&mut amount)
+        .expect("Failed to read line");
         
         // check for valid u32 values until it's fulfilled
-        loop {
-            let amount_u32: u32 = match amount.trim().parse(){
-                Ok(num) => num,
-                Err(_) => {
-                    println!("Please enter a valid number");
-                    continue;
-                }
-            };
-            println!("Got it! Added {} instances of {} to the list!",amount, item.clone());
-
-            // insert product into list
-            self.product.insert(item.trim().into(), amount_u32);
-            break;
-        }
-
+    loop {
+       let u32_am: u32 = match amount.trim().parse(){
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a valid number");
+                continue;
+            }
+        };
+        println!("Got it! Added {} instances of {} to the list!", u32_am, item);
+        // insert product into list
+        let product = Products{
+            item: item,
+            amount: u32_am
+        };
+        products.push(product);
+       break;
+       }
     }
+
+fn delete_from(products: &mut Vec<Products>){
+    
+}
+
+fn print_to_file(products: &Vec<Products>) -> std::io::Result<()>{
+    let mut file: File = File::create("shopping_list.txt")?;
+    
+    for product in products{
+        writeln!(&mut file, "{} {}", product.amount, product.item)?;
+    }   
+
+
+    Ok(())
 }
 
 fn main() {
@@ -55,12 +77,14 @@ fn main() {
 }
 
 fn user_decision(){
-    let mut products = Products{product: HashMap::new()};   
+    let mut vec = Vec::new();
+    
     
     loop {
         println!("Hello there, what would you like to do?");
         println!("1 - Add product to the list");
         println!("2 - View list");
+        println!("4 - Print to file");
         println!("Press CTRL + C");
 
         let mut decision: String = String::new();
@@ -78,10 +102,13 @@ fn user_decision(){
         };
         match decision_u32 {
             1 => {
-                products.add(); 
+                add(&mut vec); 
             }
             2 =>{
-                products.view();
+                view(&vec);
+            }
+            4 =>{
+                let _ = print_to_file(&vec);
             }
             _ => {
                 println!("Please enter a valid number");
@@ -89,9 +116,6 @@ fn user_decision(){
             }
         }
     }
-
-
-
 }
 
 
